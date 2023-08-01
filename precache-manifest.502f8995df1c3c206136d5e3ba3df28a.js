4c433c22d0286c88fbee2ce9a7af63da38e17c85
Huy Dai <huydai@mit.edu>
2023-08-01 00:14:54 -0400

    Updates

---
+++ precache-manifest.502f8995df1c3c206136d5e3ba3df28a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6364dbbf0d2c3afb68163138c3c1e787",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "f1106a1b61956a3de145",
+    "url": "/frontend/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "1c1dfe159bf817827536",
+    "url": "/frontend/static/css/main.1614cc62.chunk.css"
+  },
+  {
+    "revision": "f1106a1b61956a3de145",
+    "url": "/frontend/static/js/2.83681323.chunk.js"
+  },
+  {
+    "revision": "026b41b17b3048518fdd0ed8bb9ed94d",
+    "url": "/frontend/static/js/2.83681323.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c1dfe159bf817827536",
+    "url": "/frontend/static/js/main.4c30a23d.chunk.js"
+  },
+  {
+    "revision": "5a3179188274db41fbb9",
+    "url": "/frontend/static/js/runtime-main.1804f944.js"
+  }
+]);
